0530105e7bcdf89479c8945b6b8bdc03a2b272d0
pan3rock <panlei7@outlook.com>
2021-08-06 17:13:25 +0800

    [fix] add zmax for plotting model

--- python/plot_model.py
+++ python/plot_model.py
@@ -16,9 +16,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=msg)
     parser.add_argument('file_model', default=None, help='file of the model')
     parser.add_argument('--out', default=None, help=' output figure name')
+    parser.add_argument('--zmax', type=float, help='zmax for showing')
     args = parser.parse_args()
     file_model = args.file_model
     file_out = args.out
+    zmax = args.zmax
 
     model = np.loadtxt(file_model)
     z = model[:, 1]
@@ -28,7 +30,10 @@ if __name__ == '__main__':
 
     _, ax = plt.subplots()
     ax.step(vs, z, 'k-')
-    ax.set_ylim([0.0, z[-1]])
+    if zmax:
+        ax.set_ylim([0.0, zmax])
+    else:
+        ax.set_ylim([0.0, z[-1]])
     ax.invert_yaxis()
     ax.set_xlabel('Vs (km/s)')
     ax.set_ylabel('Depth (km)')
